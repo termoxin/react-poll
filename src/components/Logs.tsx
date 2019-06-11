398c07cf833de4eac7b26035818560f9029a2f41
Rostislav <rostislav.futornoy@gmail.com>
2019-06-11 15:53:44 +0300

    Added primitive logs

--- src/components/Logs.tsx
+++ src/components/Logs.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 import { Card } from "semantic-ui-react";
+import { read } from "../helpers/store";
 
 const SCard = styled(Card)`
   && {
@@ -9,13 +10,27 @@ const SCard = styled(Card)`
   }
 `;
 
+const ListItem = styled.li`
+  display: flex;
+  font-size: 16px;
+  margin: 10px;
+`;
+
 class Logs extends Component {
   render() {
+    const logs = read("logs");
     return (
       <SCard>
         <Card.Content>
           <Card.Header>Logs</Card.Header>
           <Card.Description>Here are your logs.</Card.Description>
+        </Card.Content>
+        <Card.Content>
+          <ul>
+            {logs.map(log => (
+              <ListItem key={log.id}>{log.name}</ListItem>
+            ))}
+          </ul>
         </Card.Content>
         <Card.Content />
       </SCard>
